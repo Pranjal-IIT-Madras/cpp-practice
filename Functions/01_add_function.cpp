e6476882ec0b263227e004184f512f5ceaf69136
Pranjal-IIT-Madras <pranjal0829@gmail.com>
2026-05-29 14:06:53 +0530

    Create 01_add_function.cpp

---
+++ Functions/01_add_function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << add(a, b) << endl;
+
+    return 0;
+}
