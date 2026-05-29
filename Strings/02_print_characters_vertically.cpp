785285c9116f04706149506983b9452763a61381
Pranjal-IIT-Madras <pranjal0829@gmail.com>
2026-05-29 14:15:57 +0530

    Create 02_print_characters_vertically.cpp

---
+++ Strings/02_print_characters_vertically.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string ok;
+
+    cin >> ok;
+
+    for (int i = 0; i < ok.length(); i++)
+    {
+        cout << ok[i] << endl;
+    }
+
+    return 0;
+}
