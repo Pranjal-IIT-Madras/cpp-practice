13c4da01b8da71061d53d4874fffe036505e01d1
Pranjal-IIT-Madras <pranjal0829@gmail.com>
2026-05-29 13:57:32 +0530

    Create 04_area_of_circle.cpp

---
+++ Basics/04_area_of_circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int radius;
+
+    cin >> radius;
+
+    float area = 3.14 * radius * radius;
+
+    cout << area << endl;
+
+    return 0;
+}
