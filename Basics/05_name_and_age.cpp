a16853f13c993436957a8619425b64eb034889ce
Pranjal-IIT-Madras <pranjal0829@gmail.com>
2026-05-29 13:58:04 +0530

    Create 05_name_and_age.cpp

---
+++ Basics/05_name_and_age.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cin >> name;
+    cin >> age;
+
+    cout << "My name is " << name
+         << " and I am "
+         << age
+         << " years old"
+         << endl;
+
+    return 0;
+}
