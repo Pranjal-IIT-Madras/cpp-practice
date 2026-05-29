9206a36f89befc015b3d89cf339839edaddbee25
Pranjal-IIT-Madras <pranjal0829@gmail.com>
2026-05-29 13:55:10 +0530

    Create 02_variables_and_datatypes.cpp

---
+++ Basics/02_variables_and_datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Pranjal";
+    int age = 18;
+    float height = 5.11;
+
+    cout << "My name is " << name << endl;
+    cout << "My age is " << age << endl;
+    cout << "My height is " << height << endl;
+
+    return 0;
+}
